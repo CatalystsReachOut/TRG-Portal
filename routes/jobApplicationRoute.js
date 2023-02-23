625a075427114a9ef06da0f2d177c53097b7568a
prkskrs <prkskrs@gmail.com>
2023-02-24 03:55:53 +0530

    get all applicants::applicants ::added

---
+++ routes/jobApplicationRoute.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+
+// import controllers
+import {
+  jobApplication,
+  getQuestionsJobId,
+  getAllApplicant,
+} from "../controllers/jobApplication.js";
+
+// import userMiddlewares
+import { isLoggedIn } from "../middlewares/jobSeekerMiddleware.js";
+
+// routes
+router.route("/jobApplication/:jobId").post(isLoggedIn, jobApplication);
+router.route("/jobApplication/questions/:jobId").get(isLoggedIn, getQuestionsJobId);
+router.route("/jobApplication/applicant").get(getAllApplicant);  // for admin
+
+export default router;
